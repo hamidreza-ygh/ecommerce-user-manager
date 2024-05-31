e79a6b60782f8c303dfcdc3149ebbca6def5ace6
Hamidreza-ygh <hr.yaghoubi@gmail.com>
2024-05-31 13:40:15 +0200

    Change CORS Configuration

--- auth/src/app.js
+++ auth/src/app.js
@@ -50,7 +50,7 @@ class App {
     //     next();
     //   }
     // });
-    app.use(function(req, res, next) {
+    this.app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific methods
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-User'); // Allow specific headers
